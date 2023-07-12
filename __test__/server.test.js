34e895c509076ea88e0308214c2131d2d69ca35b
Byneros <ing.nelsonjulio@gmail.com>
2023-07-12 00:52:40 -0500

    Update server.test.js

--- __test__/server.test.js
+++ __test__/server.test.js
@@ -4,4 +4,4 @@ const app = express();
 app.use(express.json());
 app.use('/tarea', tareasRoutes);
 const server = app.listen( () => console.log(`Server running on test mode`));
-module.exports = server;
+module.exports = server; 
