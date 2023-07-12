49b4946dfef6e581f53cdaecb7e688d4c62a5218
Byneros <ing.nelsonjulio@gmail.com>
2023-07-12 00:08:17 -0500

    commit

---
+++ __test__/crearTarea.js
@@ -0,0 +1,48 @@
+const supertest = require('supertest');
+const mongoose = require('mongoose');
+const { MongoMemoryServer } = require('mongodb-memory-server');
+const server = require('./server');
+
+let mongoServer;
+
+beforeAll(async () => {
+    mongoServer = await MongoMemoryServer.create();
+    const mongoUri = mongoServer.getUri();
+    await mongoose.connect(mongoUri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+});
+
+afterAll(async () => {
+    await mongoose.connection.close();
+    await mongoServer.stop();
+    await new Promise(resolve => server.close(resolve));
+});
+
+describe('POST /', () => {
+    it('should create a new task and return 201 status', async () => {
+        const data = {
+            nombre: 'Tarea de pruebas Task',
+            fechaLimite: getFechaFuturo(),
+            estado: 'creada',
+        };
+
+        await supertest(server)
+            .post('/tarea')
+            .send(data)
+            .expect(201)
+            .then((response) => {
+                expect(response.body.nombre).toBe(data.nombre);
+                expect(new Date(response.body.fechaLimite).toISOString().split('.')[0] + "Z").toBe(data.fechaLimite);
+                expect(response.body.estado).toBe(data.estado);
+            });
+    });
+});
+
+const getFechaFuturo = () => {
+    const currentDate = new Date();
+    const futureDate = new Date(currentDate.setDate(currentDate.getDate() + 5));
+    const futureDateISO = futureDate.toISOString().split('.')[0] + "Z";
+    return futureDateISO;
+};
