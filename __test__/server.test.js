c10ff72f558f936713c1f2e30962073872af5f1a
Byneros <ing.nelsonjulio@gmail.com>
2023-07-12 00:52:23 -0500

    commit

--- __test__/server.test.js
+++ __test__/server.test.js
@@ -1,11 +1,7 @@
-//__test__/server.js
-// Importa Express, que es una biblioteca de Node.js utilizada para construir aplicaciones web.
 const express = require('express');
-// Importa las rutas de tareas que hemos definido anteriormente en un router de Express.
 const tareasRoutes = require('../routes/tarea');
-// Crea una nueva aplicación Express.
 const app = express();
 app.use(express.json());
-app.use('/tarea', tareasRoutes); // Usar las rutas de tareas
+app.use('/tarea', tareasRoutes);
 const server = app.listen( () => console.log(`Server running on test mode`));
-module.exports = server; // Asegúrate de exportar la aplicación
+module.exports = server;
--- server.js
+++ server.js
@@ -1,18 +1,12 @@
-const express = require('express');
-const cors = require('./config/cors');
-const conectarDB = require('./config/db');
-const tareasRoutes = require('./routes/tarea');
-const { use } = require('./routes/routes.tareaController');
-
+const express = require("express");
+const conectarDB = require("./Config/db");
+const RutaEmpleado =  require("./routes/routes.tareaController");
 const app = express();
+const cors = require('./Backend/Config/cors');
 
-conectarDB();
-
-app.use(cors);
 app.use(express.json());
-
-app.use('/tarea', tareasRoutes);
-
+app.use(cors);
+conectarDB();
+app.use('/tareaController', tareasRoutes);
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => console.log(`Server running  on port ${PORT}`));
